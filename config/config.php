bd9e48202916fcea854bcde07d8cbd0380004cfe
enormand <normand.erwan@gmail.com>
2014-05-17 15:11:07 +0200

    Add config/

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Path definitions */
+$path = array('controller' => '/controllers',
+			'model' => '/models',
+			'library' => '/libraries',
+			'view' => '/views',
+			'css' => '/ressources/css',
+			'javascript' => '/ressources/javascript',
+			'image' => '/ressources/images');
+			
+			
+/* Connection informations */
+$connection = array('server' => 'localhost',
+					'username' => 'root',
+					'password' => 'qds',
+					'database' => '');
