61f3c9c2cc2a96eb0e1a64525a321eba6f6eb363
misterj <duthon.jean@gmail.com>
2014-06-11 23:21:06 +0200

    Ajout vue Ajouts
    Ajout des vues ajout entreprise et agent codées par Iris

---
+++ view/ajout_entreprise.php
@@ -0,0 +1,25 @@
+
+	<div class="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+		<form class="form-horizontal" method="post" action="" role="form">
+			<h2 class="page-header">Ajouter une nouvelle entreprise</h2>
+				<div class="form-group">
+					<label for="nom" class="col-sm-2 control-label">Nom</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="nom" id="nom" placeholder="nom" required autofocus>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label for="fonction" class="col-sm-2 control-label">Fonction</label>
+					<div class="col-sm-10">
+						<input type="text" class="form-control" name="fonction" id="fonction" placeholder="fonction" required>
+					</div>
+				</div>
+				
+			
+				<div class="form-group text-right">
+				<button type="submit" class="btn btn-primary">Ajouter</button>
+				</div>
+			</div>
+		</form>
+	</div>
---
+++ view/ajouter_agent.php
@@ -0,0 +1,30 @@
+<div class="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1">
+	<form class="form-horizontal" method="post" action="" role="form">
+		<h2 class="page-header">Ajouter un nouvel agent</h2>
+			<div class="form-group">
+				<label for="identifiant" class="col-sm-2 control-label">Identifiant</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" name="identifiant" id="identifiant" placeholder="identifiant" required autofocus>
+				</div>
+			</div>
+			
+			<div class="form-group">
+				<label for="mot de passe" class="col-sm-2 control-label">Mot de passe</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" name="mot de passe" id="mot de passe" placeholder="mot de passe" required>
+				</div>
+			</div>
+			
+			<div class="form-group">
+				<label for="fonction" class="col-sm-2 control-label">Fonction</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" name="fonction" id="fonction" placeholder="fonction" required>
+				</div>
+			</div>
+		
+			<div class="form-group text-right">
+			<button type="submit" class="btn btn-primary">Créer</button>
+			</div>
+		</div>
+	</form>
+</div>
